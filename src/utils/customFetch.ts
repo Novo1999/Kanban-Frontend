ddf92bede0a80a0a057130a3df1c4ecec970fa18
Novodip Mondal <neptune.navo@gmail.com>
2023-11-28 21:28:19 +0600

    Update customFetch.ts

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+// 'https://kanbann.up.railway.app/api/v1'
 const customFetch = axios.create({
-  baseURL: 'https://kanbann.up.railway.app/api/v1',
+  baseURL: 'https://kanban-backend-novo.vercel.app/api/v1',
   withCredentials: true,
 })
 
