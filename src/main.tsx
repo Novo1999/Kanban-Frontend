1dbb1ad5fefbbdfe75c8c29e2ffbfc85c99c0dbe
Novo1999 <neptune.navo@gmail.com>
2025-05-26 13:48:11 +0600

    remove react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import { Error, KanbanBoard, Login, Register, Settings } from './pages/index.ts'
 import { loader as loginLoader } from './pages/Login.tsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import KanbanContent from './pages/KanbanContent.tsx'
 
